b447cf4ee0f62ec18de66d3154e9cd4c9a8e861e
dipansrimany <dipansrimany@gmail.com>
2024-07-16 01:22:55 +0530

    Activity 5 and Task 6 are done

---
+++ Day-1/Activity5.js
@@ -0,0 +1,10 @@
+// Task6 (Understanding Const)
+
+const constant = 6;
+
+console.log(constant);
+
+constant=3;
+console.log(constant);
+
+let error = "constant=3; TypeError: Assignment to constant variable."
