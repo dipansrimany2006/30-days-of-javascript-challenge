6b959d31187709d4080cf5cf8cb71eb7bfa88f9f
dipansrimany <dipansrimany@gmail.com>
2024-07-16 01:06:15 +0530

    Activity 3 and Task 4 are done

---
+++ Day-1/Activity3.js
@@ -0,0 +1,19 @@
+//Task 4
+
+let num1 = 12;
+let name = "Jack Willson";
+const decesion = true;
+
+const object ={
+    course: "30 Days JS Challenge",
+    fees: 0,
+    builder: "Chai Aur Code"
+}
+
+let arr = [45, "Hitesh Sir", true]
+
+console.log(num1);
+console.log(name);
+console.log(decesion);
+console.log(object);
+console.log(arr);
