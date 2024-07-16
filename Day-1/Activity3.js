e76943e7cbbeb1b88872e6423bdc1f6c296a3721
dipansrimany <dipansrimany@gmail.com>
2024-07-16 08:28:44 +0530

    Activity 3's pending task completed

--- Day-1/Activity3.js
+++ Day-1/Activity3.js
@@ -13,7 +13,16 @@ const object ={
 let arr = [45, "Hitesh Sir", true]
 
 console.log(num1);
+console.log(typeof(num1));
+
 console.log(name);
+console.log(typeof(name));
+
 console.log(decesion);
+console.log(typeof(decesion));
+
 console.log(object);
+console.log(typeof(object));
+
 console.log(arr);
+console.log((typeof(arr)));
