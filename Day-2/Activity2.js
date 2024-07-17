df7f28765f2dc7501067262c0b113516d48b9f36
dipansrimany <dipansrimany@gmail.com>
2024-07-17 21:44:49 +0530

    Activity 2 and Task 6 & 7 updated

---
+++ Day-2/Activity2.js
@@ -0,0 +1,16 @@
+//Assignment Operators
+
+let variable = 12;
+
+let number = 8;
+
+//now add with assignment operator
+variable += number;
+console.log(variable);
+
+//keep the value unchanged
+variable=12;
+
+// now substract using assignment operator
+variable -= number;
+console.log(variable);
