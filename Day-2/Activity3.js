9c716a23bc416afbd32c9ecd97c58287d3eeba78
dipansrimany <dipansrimany@gmail.com>
2024-07-18 13:18:39 +0530

    Activity 3 is done and Task 10 is completed

---
+++ Day-2/Activity3.js
@@ -0,0 +1,25 @@
+//Comparison Operator
+
+//Task 8 (comparison operator)
+
+let num1 = 456, num2 = 852;
+
+num1>num2?console.log(`${num1} is greater than ${num2}`):console.log(`${num1} is less than ${num2}`);
+
+let num3 = 693 , num4 = 452;
+
+num3>num4?console.log(`${num3} is greater than ${num4}`):console.log(`${num3} is less than ${num4}`);
+
+
+//Task 9
+
+let num5 = 453, num6 = 453
+num5<=num6?console.log(`${num5} is same or smaller than ${num6}`):console.log(`${num6}is greater than ${num6} `);
+
+//Task 10
+
+let  num7 = 78, num8= '78';
+num7==num8?console.log(`${num7} is equal to the ${num8}, but their data type is not checked`):console.log(`${num7} is equal to the ${num8}, and  their data type is also same`);
+
+let num9 = 'name', num10='name';
+num9===num10?console.log(`${num9} is equal to the ${num10}, and  their data type is also same`):console.log(`${num9} and ${num10} is not same in value or their value is not same.`);;
