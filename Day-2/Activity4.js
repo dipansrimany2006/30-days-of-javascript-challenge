570c5e5d2baac625fd5d0e33e863b59308cb932b
dipansrimany <dipansrimany@gmail.com>
2024-07-20 18:39:29 +0530

    Activity 4 Task 11 internal function variable name updated

--- Day-2/Activity4.js
+++ Day-2/Activity4.js
@@ -3,7 +3,7 @@
 let var1 = 8, var2 = 5;
 
 function andOperatorCheckor(a,b){
-    if(var1==8 && var2==5){
+    if(a == 8 && b == 5){
         console.log('Both contidions are correct');
     }else{
         console.log('Any one condition is correct or none are correct');
@@ -16,6 +16,4 @@ andOperatorCheckor(var1,var2);
 var1 = 5, var2 = 3;
 
 andOperatorCheckor(var1,var2);
-
-//Task 12
 
