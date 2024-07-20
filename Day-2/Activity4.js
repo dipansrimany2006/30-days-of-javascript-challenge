4aa390740db34e20d9ee6529a6f944c71ca8d970
dipansrimany <dipansrimany@gmail.com>
2024-07-20 17:57:46 +0530

    Activity 4 Task 11 done

---
+++ Day-2/Activity4.js
@@ -0,0 +1,18 @@
+// Task 11
+
+let var1 = 8, var2 = 5;
+
+function logicalOperatorCheckor(a,b){
+    if(var1==8 && var2==5){
+        console.log('Both contidions are correct');
+    }else{
+        console.log('Any one condition is correct or none are correct');
+    }
+}
+
+logicalOperatorCheckor(var1,var2);
+
+
+var1 = 5, var2 = 3;
+
+logicalOperatorCheckor(var1,var2);
