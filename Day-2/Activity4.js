d0a1b971c4488ab8d20a026d22e8053e3eaad9b3
dipansrimany <dipansrimany@gmail.com>
2024-07-21 13:11:41 +0530

    Activity 4 Functions name spelling corrected

--- Day-2/Activity4.js
+++ Day-2/Activity4.js
@@ -3,7 +3,7 @@
 let var1 = 8,
   var2 = 5;
 
-function andOperatorCheckor(a, b) {
+function andOperatorChecker(a, b) {
   if (a == 8 && b == 5) {
     console.log("Both contidions are correct");
   } else {
@@ -11,11 +11,11 @@ function andOperatorCheckor(a, b) {
   }
 }
 
-andOperatorCheckor(var1, var2);
+andOperatorChecker(var1, var2);
 
 (var1 = 5), (var2 = 3);
 
-andOperatorCheckor(var1, var2);
+andOperatorChecker(var1, var2);
 
 //Task 12  (Or operator)
 
