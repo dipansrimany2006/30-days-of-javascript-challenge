1431ed8e1d1c49388664e43ee679339a95ec7583
dipansrimany <dipansrimany@gmail.com>
2024-07-21 11:58:32 +0530

    Activity 4 Task 12 completed

--- Day-2/Activity4.js
+++ Day-2/Activity4.js
@@ -12,8 +12,19 @@ function andOperatorCheckor(a,b){
 
 andOperatorCheckor(var1,var2);
 
-
 var1 = 5, var2 = 3;
 
 andOperatorCheckor(var1,var2);
 
+//Task 12  (Or operator)
+
+function orOperator(a,b){
+if(a==5||b==7){
+    console.log('Any one is correct or Both conditions are correct');
+}else{
+    console.log('None are correct');
+}
+}
+
+orOperator(8,6);
+orOperator(5,9);
