41dcef32ef30380d4f1230fc1e42ffb2cbc93888
dipansrimany <dipansrimany@gmail.com>
2024-07-21 18:42:50 +0530

    Feature Request Completed

---
+++ Day-2/featureRequest.js
@@ -0,0 +1,20 @@
+//Arithmatic Operations Script
+
+const arithmaticOperation = (a,b)=>{
+//addition
+console.log(`${a} + ${b} = ${a+b}`);
+//substraction
+console.log(`${a} - ${b} = ${a-b}`);
+//multiplication
+console.log(`${a} * ${b} = ${a*b}`);
+//division
+console.log(`${a} / ${b} = ${a/b}`);
+//remainder
+console.log(`${a} % ${b} = ${a%b}`);
+}
+
+arithmaticOperation(45,78);
+
+//Comparison and logical Operator scripts already done in activity 4
+
+// Ternery Operator already done in activity 5
