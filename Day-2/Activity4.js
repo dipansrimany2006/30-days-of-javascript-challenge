f573565e60411e50e301a4d4aa42815f04bcfa1d
dipansrimany <dipansrimany@gmail.com>
2024-07-21 12:41:57 +0530

    Activity 4 Task 13 completed

--- Day-2/Activity4.js
+++ Day-2/Activity4.js
@@ -1,30 +1,42 @@
 // Task 11
 
-let var1 = 8, var2 = 5;
+let var1 = 8,
+  var2 = 5;
 
-function andOperatorCheckor(a,b){
-    if(a == 8 && b == 5){
-        console.log('Both contidions are correct');
-    }else{
-        console.log('Any one condition is correct or none are correct');
-    }
+function andOperatorCheckor(a, b) {
+  if (a == 8 && b == 5) {
+    console.log("Both contidions are correct");
+  } else {
+    console.log("Any one condition is correct or none are correct");
+  }
 }
 
-andOperatorCheckor(var1,var2);
+andOperatorCheckor(var1, var2);
 
-var1 = 5, var2 = 3;
+(var1 = 5), (var2 = 3);
 
-andOperatorCheckor(var1,var2);
+andOperatorCheckor(var1, var2);
 
 //Task 12  (Or operator)
 
-function orOperator(a,b){
-if(a==5||b==7){
-    console.log('Any one is correct or Both conditions are correct');
-}else{
-    console.log('None are correct');
-}
+function orOperator(a, b) {
+  if (a == 5 || b == 7) {
+    console.log("Any one is correct or Both conditions are correct");
+  } else {
+    console.log("None are correct");
+  }
 }
 
-orOperator(8,6);
-orOperator(5,9);
+orOperator(8, 6);
+orOperator(5, 9);
+
+//Task 13 (Not Operator)
+
+function notOperator(a) {
+  console.log(`The value of the variable is ${a}`);
+  a = !a;
+  console.log(`Now the value is ${a}`);
+}
+
+notOperator(true);
+notOperator(0);
