c89b9e81b99ed80e44354aab25ef048afddcae8a
dipansrimany <dipansrimany@gmail.com>
2024-07-22 17:16:28 +0530

    Day3 Activity 1 Task 2 Completed

--- Day-3/Activity1.js
+++ Day-3/Activity1.js
@@ -13,3 +13,19 @@ function positivenegativeChecker(a){
 positivenegativeChecker(4);
 positivenegativeChecker(0);
 positivenegativeChecker(-85);
+
+//Task 2 (Age checker)
+
+const ageChecker=(age)=>{
+if(age>18){
+console.log("Your age is greater than 18");
+}else if(age==18){
+    console.log("Your age is equal to 18");
+}else{
+    console.log("Your age is less than 18");
+}
+}
+
+ageChecker(78);
+ageChecker(18);
+ageChecker(5);
