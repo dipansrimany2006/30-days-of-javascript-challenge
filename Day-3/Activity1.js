fd1ffee1ae0655046b63e4f55900d219ab1931de
dipansrimany <dipansrimany@gmail.com>
2024-07-22 17:08:44 +0530

    Day3 Activity 1 Task 1 Completed

---
+++ Day-3/Activity1.js
@@ -0,0 +1,15 @@
+//Task 1
+
+function positivenegativeChecker(a){
+    if (a>0) {
+        console.log(`${a} is a positive number`);
+    } else if(a==0){
+        console.log(`${a} is equal as 0`);
+    } else{
+        console.log(`${a} is a negative number`);
+    }
+}
+
+positivenegativeChecker(4);
+positivenegativeChecker(0);
+positivenegativeChecker(-85);
