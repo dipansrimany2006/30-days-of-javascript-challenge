fba3654127df9ec8e70ded7552946f4eb5e27adf
dipansrimany <dipansrimany@gmail.com>
2024-07-23 11:55:02 +0530

    Activity 4 Task 6 Completed

---
+++ Day-3/Activity4.js
@@ -0,0 +1,10 @@
+// Conditional Operator 
+
+//Task 6 (Check the input numbers are odd or even)
+
+function oddEvenChecker(number){
+number%2==0?console.log(`Your input number, ${number} is even number`):console.log(`Your input number, ${number} is odd number`);
+}
+
+oddEvenChecker(45);
+oddEvenChecker(12);
