3f878e34c78b793b4bbb74e4ffc19020fff99ba4
dipansrimany <dipansrimany@gmail.com>
2024-07-27 18:08:09 +0530

    Activity 3 Task 6 Completed

--- Day-4/Activity3.js
+++ Day-4/Activity3.js
@@ -6,3 +6,20 @@ do {
     console.log(number);
     number++;
 } while (number<=5);
+
+// task 6 (Calculate the  factorial number using do while loop)
+
+
+function factorial(n) {
+    let result = 1;
+    let i = 1;
+  
+    do {
+      result *= i;
+      i++;
+    } while (i <= n);
+  
+    return result;
+  }
+  
+  console.log(factorial(5)); // Output: 120
