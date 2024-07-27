d997378e5525ed26f85c6d0bbaf7afa8f72a197c
dipansrimany <dipansrimany@gmail.com>
2024-07-27 14:16:30 +0530

    Activity 1 Task 2 Completed

--- Day-4/Activity1.js
+++ Day-4/Activity1.js
@@ -3,3 +3,9 @@
 for (let index = 1; index <=10; index++) {
     console.log(index);
 }
+
+//Task 2 (make a multiplication table of 5 using loop)
+
+for (let i = 1; i <= 10; i++) {
+    console.log(5*i);
+}
